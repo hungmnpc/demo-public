53c4bcf4af687ef0f11f795064b4f03d96b6ee56
20020421 <hungkaiken2@gmail.com>
2021-09-09 16:09:37 +0700

    first commit

---
+++ start.js
@@ -0,0 +1,12 @@
+window.addEventListener("load", function (){
+    const slider = this.document.querySelector(".slider");
+    const sliderMain = this.document.querySelector(".slider-main");
+    const sliderItems = this.document.querySelectorAll(".slider-item");
+    const prevBtn = this.document.querySelector(".slider-prev");
+    const nextBtn = this.document.querySelector(".slider-next");
+    const dotItem = this.document.querySelectorAll(".slider-dot-item");
+    const sliderItemWidth = sliderItems[0].offsetWidth;
+    const slidersLength = sliderItems.length;
+    
+    
+});
