a6a397e79acb5831878ed037a7d4072eb3afa30e
20020421 <hungkaiken2@gmail.com>
2021-09-10 18:31:02 +0700

    1

--- start.js
+++ start.js
@@ -7,6 +7,29 @@ window.addEventListener("load", function (){
     const dotItem = this.document.querySelectorAll(".slider-dot-item");
     const sliderItemWidth = sliderItems[0].offsetWidth;
     const slidersLength = sliderItems.length;
+    let index = 0;
+    // sliderMain.style.transform = "translateX(-2000px)";
+    
+    nextBtn.onclick = function(){
+        index++;
+        index= index%slidersLength;
+        let lengthtransform  = index * sliderItemWidth * -1;
+        console.log(lengthtransform)
+        sliderMain.style.transform = `translate(${lengthtransform}px)`;
+    }
+    prevBtn.onclick = function(){
+        index--;
+        if (index === -1){
+            index = slidersLength -1;
+        }
+        console.log(index);
+        let lengthtransform  = Math.abs(index)%slidersLength * sliderItemWidth * -1;
+        console.log(lengthtransform)
+        sliderMain.style.transform = `translate(${lengthtransform}px)`;
+    }
+
+   
+
     
     
 });
